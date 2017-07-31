394adaaf04b4143c37c28fa07c0b74356e6b1701
Alexandr Köln <koelnfrei@gmail.com>
2017-07-31 17:33:55 +0300

    fix rounding

--- Handlers/handler.py
+++ Handlers/handler.py
@@ -9,8 +9,8 @@ class MainHandlers:
 
     @staticmethod
     def btc(bot, update):
-        btc_price = CoinRate.getRate("btc")
-        eth_price = CoinRate.getRate("eth")
+        btc_price = CoinRate.get_rate("btc")
+        eth_price = CoinRate.get_rate("eth")
         bot.send_message(chat_id=update.message.chat_id,
                          text="1 BTC = " + btc_price + "\n" +
                          "1 ETH = " + eth_price)
