7ccd300ab02c2bfb7a987e6707e6991814047ef4
Alexandr Köln <koelnfrei@gmail.com>
2017-07-31 21:29:33 +0300

    fixing troubles

--- Handlers/handler.py
+++ Handlers/handler.py
@@ -7,7 +7,7 @@ class MainHandlers:
     @staticmethod
     def start(bot, update):
         bot.send_message(chat_id=update.message.chat_id,
-                         text="I'm bot, and I'm not ready yet, but I deployed with docker-compose =_=")
+                         text="I'm bot, and I'm not ready yet =_=")
 
     @staticmethod
     def btc(bot, update):
