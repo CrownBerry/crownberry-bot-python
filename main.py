d6eb4b0fa7d2eb8529aba0efb3948ca9749f566f
Alexandr Köln <koelnfrei@gmail.com>
2017-07-31 11:50:09 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+import logging
+
+updater = Updater(token="420905549:AAHb1OstzqlLiwGQegNGP27UO1uoS4Jpw5g")
+dispatcher = updater.dispatcher
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="I'm bot, and I'm not ready yet")
+
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+if __name__ == '__main__':
+    updater.start_polling()
