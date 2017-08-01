7edab123d1466e659c696557742c9508cef5adaa
Alexandr Köln <koelnfrei@gmail.com>
2017-08-01 10:44:31 +0300

    Added configparser

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
+import configparser
+
+
 def init():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
     global model
+    global tg_token
+    tg_token = config['DEFAULT']['tgToken']
--- main.py
+++ main.py
@@ -4,22 +4,22 @@ from Handlers.handler import MainHandlers
 from Services.MyOwnCNN import MyOwnCNN
 import config
 
-updater = Updater(token="420905549:AAHb1OstzqlLiwGQegNGP27UO1uoS4Jpw5g")
-dispatcher = updater.dispatcher
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-
-start_handler = CommandHandler('start', MainHandlers.start)
-btc_handler = CommandHandler('btc', MainHandlers.btc)
-cod_handler = MessageHandler(Filters.photo, MainHandlers.cat_or_dog)
-
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(btc_handler)
-dispatcher.add_handler(cod_handler)
-
 
 if __name__ == '__main__':
     config.init()
     MyOwnCNN.init_model()
+
+    updater = Updater(token=config.tg_token)
+    dispatcher = updater.dispatcher
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+
+    start_handler = CommandHandler('start', MainHandlers.start)
+    btc_handler = CommandHandler('btc', MainHandlers.btc)
+    cod_handler = MessageHandler(Filters.photo, MainHandlers.cat_or_dog)
+
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(btc_handler)
+    dispatcher.add_handler(cod_handler)
+
     updater.start_polling()
