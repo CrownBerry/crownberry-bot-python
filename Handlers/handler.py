8ea1c6d2baee35fa017f13de821810ef30e047fa
Alexandr Köln <koelnfrei@gmail.com>
2017-08-02 14:29:11 +0300

    Trying to fix buildng

--- Handlers/handler.py
+++ Handlers/handler.py
@@ -7,7 +7,7 @@ class MainHandlers:
     @staticmethod
     def start(bot, update):
         bot.send_message(chat_id=update.message.chat_id,
-                         text="I'm bot, and I'm not ready yet =_=")
+                         text="I'm bot and I can tell you current exchange rate of BTC v0.01")
 
     @staticmethod
     def btc(bot, update):
