270a51a23e18b19aa5dcfec49fa20dc603a04445
CrownBerry <koelnfrei@gmail.com>
2017-08-11 10:30:58 +0300

    Remove non-lettaer character from messages

--- Handlers/handler.py
+++ Handlers/handler.py
@@ -31,7 +31,7 @@ def cat_or_dog(bot, update):
 def topword_saving(bot, update):
     user = update.message.from_user.username
     message = update.message.text
-    message = ''.join(c for c in message if c.isalpha())
+    message = ''.join(c if c.isalpha() else ' ' for c in message)
     for word in message.split():
         if len(word) > 2:
             config.topwords.add_word(word, user)
